da1889e924cf28cad77500fac639299c2e7003c8
chaseeddie <38984786+chaseeddie@users.noreply.github.com>
2019-02-22 21:15:40 -0600

    Update tab-historical migration-needs work.py

--- tab-historical migration-needs work.py
+++ tab-historical migration-needs work.py
@@ -1,44 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 20 17:18:18 2019
-
-@author: Danielle
-"""
 import pandas as pd
 
-
-
-
 mi=pd.read_excel('tab-a-1.xlsx', 
-                         skiprows=4, 
+                         skiprows=6, 
                        skipfooter=12,                   
-                       na_values = "(NA)",
-                       header=[0,1,2],
-                       index_col = 0)       
-
-
-
+                       na_values = "",
+                       header=[0],
+                       index_col = [0,1,2,3,4,5,6,7,8])       
 
 
 
 mi.dropna(how='all', inplace=True) 
-mi = mi.stack().stack()       
-mi= mi.reset_index()
+mi = mi.stack().reset_index()
            
-mi.rename(columns={mi.columns[0]: 'Years',
-                         mi.columns[1]: 'Type of residence in the United States',
-                         mi.columns[2]: 'Different or Same County',
-                         mi.columns[3]: 'Different or Same State',
-                         mi.columns[4]: 'total',
-                         }, inplace=True)                      
-                       
-                       
-                       
-                       
-
+mi.rename(columns={mi.columns[0]: 'Mobility Period',
+                   mi.columns[1]: 'Total, 1 year old and over',
+                   mi.columns[2]: 'Same residence',
+                   mi.columns[3]: 'Total movers',
+                   mi.columns[4]: 'Different residence total',
+                   mi.columns[5]: 'Different residence same county',
+                   mi.columns[6]: 'Different county total',
+                   mi.columns[7]: 'Different county same state',
+                   mi.columns[8]: 'Different county different state',
+                   mi.columns[9]: 'Drop5',
+                   mi.columns[10]: 'Movers from abroad'
+                   }, inplace=True)
+mi = mi.drop('Drop5', 1)                                      
+mi = mi.round(decimals=1)
 mi.to_excel(excel_writer='migration_clean.xlsx',           
-                sheet_name='migration',                           
-                 
+                sheet_name='migration',                                     
 index=False
-                                
-                )   
+                )
